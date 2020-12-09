b7e07b33065714e050228fbffebad3f3fe037689
“saschakala” <mxsaschakala@gmail.com>
2020-12-09 15:09:32 -0500

    added bricks

--- src/canvas.js
+++ src/canvas.js
@@ -14,6 +14,22 @@ var rightPressed = false;
 var leftPressed = false;
 var interval = setInterval(draw, 10);
 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
@@ -25,13 +41,33 @@ function drawBall() {
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#c04df9";
     ctx.fill();
     ctx.closePath();
 }
 
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.fillStyle = "#f3ea5f";
+            ctx.fillRect(brickX, brickY, brickWidth, brickHeight);
+            
+            ctx.strokeStyle = "#ff48c4";
+            ctx.lineWidth = 2;
+            ctx.strokeRect(brickX, brickY, brickWidth, brickHeight);
+            ctx.closePath();
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    drawBricks();
     drawBall();
     drawPaddle();
 
