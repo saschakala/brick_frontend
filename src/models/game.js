fb258c8caca6830e5531a6462433e95c11cd200e
“saschakala” <mxsaschakala@gmail.com>
2020-12-27 10:36:21 -0500

    fixed score table styling with headers

--- src/models/game.js
+++ src/models/game.js
@@ -8,7 +8,7 @@ class Game{
 
     renderGame(){
         return `
-            <tr data-id=${this.id}>
+            <tr data-id=${this.id} style="text-align: left">
                 <td>${this.userName}</td>
                 <td>${this.score}</td>
             </tr>`;
